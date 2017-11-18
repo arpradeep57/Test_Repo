9521ced533f79622f635c96d34613960150cc266
arpradeep57 <a.rameshpradeep@gmail.com>
2017-11-18 01:41:18 -0600

    mistake rectified

--- HelloClass.java
+++ HelloClass.java
@@ -3,7 +3,7 @@ public class HelloClass{
    public static void main(String[] args){
    for(int i=0; i<10; i++){
    System.out.println("Value is: "+i);
-   system.out,println("Hello, world!"+i);
+   system.out.println("Hello, world!"+i);
    }
    
    }
